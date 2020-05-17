51267509c47b43ef24f8016a54fd450456bb2e8d
vheble <vishal.heble@narvar.com>
2020-05-17 17:22:27 +0530

    Added components for fish store

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const NotFound = () => (
+        <div>
+            <h2 style={{paddingTop:20}}>Not Found!!!</h2>
+        </div>
+    );
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 export class StorePicker extends React.Component {
+    myInput = React.createRef();
+     goToStore = event => {
+        event.preventDefault();
+        const storeName = this.myInput.current.value;
+        this.props.history.push(`/store/${storeName}`);
+    };
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Enter store name</h2>
-                <input type="text" placeholder="store name" />
-                <button type="submit">Visit</button>
+                <input 
+                    ref={this.myInput}
+                    type="text" 
+                    placeholder="store name" 
+                    defaultValue={getFunName()}/>
+                <button type="submit" >Visit</button>
             </form>
         )
     };
